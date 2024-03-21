6c318075850576fd6eefb83f4a339b0e09b2c6a7
mariadelosangelesmagaz <mariadelosangelesmagaz@gmail.com>
2024-03-21 19:32:26 -0300

    rutas creadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import { routes } from './Components/utils/routes';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Routes/Home'
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
+import Favs from './Routes/Favs'
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path={routes.home} element={<Home/>}/>
+        <Route path={routes.contact} element={<Contact/>}/>
+        <Route path={routes.detail} element={<Detail/>}/>
+        <Route path={routes.favs} element={<Favs/>}/>
+        <Route path="*" element={<h1>Página no encontrada. ERROR 404</h1>} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
