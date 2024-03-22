9e46e3e2d9dd8cfa7121a50b72b0cd582879eb43
Agosducret <agos.ducret18@gmail.com>
2024-03-21 21:40:40 -0300

    Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [contacto, setContacto] = useState({
+    nombre: "",
+    correo: "",
+  });
+  const [mostrar, setMostrar] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (contacto.nombre.length >= 5) {
+      setMostrar(true);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
 
   return (
     <div>
-      <form>
-      </form>
+      {mostrar ? null : (
+        <form onSubmit={handleSubmit}>
+          <label>Nombre:</label>
+          <input
+            type="text"
+            onChange={(event) =>
+              setContacto({ ...contacto, nombre: event.target.value })
+            }
+          />
+          <label>Correo Electronico:</label>
+          <input
+            role="email"
+            type="email"
+            onChange={(event) =>
+              setContacto({ ...contacto, correo: event.target.value })
+      
+            }
+        
+        />
+          <button>Enviar</button>
+        </form>
+      )}
+
+      {mostrar ? (
+        <h2>
+          Gracias {contacto.nombre}, te contactaremos cuando antes vía mail.
+        </h2>
+      ) : null}
+      {error && <h3>Por favor verifique su información nuevamente.</h3>}
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Link} from 'react-router-dom'
+import { routes } from "./utils/routes";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <>
+        <Link to={routes.home}><h3>Home</h3></Link>
+        <Link to={routes.contact}><h3>Contact</h3></Link>
+        <Link to={routes.favs}><h3>Favs</h3></Link>
+        <Link to={routes.detail}><h3>Detail</h3></Link>
+      </>
+
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
